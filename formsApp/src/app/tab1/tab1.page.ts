6eb8c85befcd50172e5cfe1d7d914a10f27b064f
Gabriel Henrique dos Santos <gabrielHsantos2018@gmail.com>
2022-12-05 08:56:16 -0300

    arrumando

--- formsApp/src/app/tab1/tab1.page.ts
+++ formsApp/src/app/tab1/tab1.page.ts
@@ -6,36 +6,33 @@ import { StorageService } from '../services/storage.service';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-
   listaUsuarios: Usuario[] = [];
   listaProdutos: Produto[] = [];
-  constructor(private storageService: StorageService) { }
+  constructor(private storageService: StorageService) {}
 
-  async buscarUsuarios(){
+  async buscarUsuarios() {
     this.listaUsuarios = await this.storageService.getAll();
   }
 
-ionViewDidEnter() {
-  this.buscarUsuarios();
-}
+  async excluirCadastro(email: string) {
+    await this.storageService.remove(email);
+    this.buscarUsuarios();
+  }
 
- async excluirCadastro(email: string) {
-  await this.storageService.remove(email);
-  this.buscarUsuarios();
-}
-async buscarProduto(){
-  this.listaProdutos = await this.storageService.getAll();
-}
+  async buscarProduto() {
+    this.listaProdutos = await this.storageService.getAll();
+    console.log( this.listaProdutos);
+  }
 
-ionViewDidEnter1() {
-this.buscarProduto();
-}
+  ionViewDidEnter() {
+    this.buscarProduto();
+  }
 
-async excluirProduto(nome: string) {
-await this.storageService.remove(nome);
-this.buscarProduto();
-}
+  async excluirProduto(nome: string) {
+    await this.storageService.remove(nome);
+    this.buscarProduto();
+  }
 }
